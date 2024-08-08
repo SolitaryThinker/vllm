6921f1ceeabefbdb26421fc8426d267e4c6427d1
Will Lin <wlsaidhi@gmail.com>
2024-08-07 17:26:19 -0700

    rebase bugs

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -50,7 +50,7 @@ from vllm.prompt_adapter.worker_manager import (
 from vllm.sampling_params import SamplingParams
 from vllm.sequence import (IntermediateTensors, SamplerOutput,
                            SequenceGroupMetadata)
-from vllm.utils import (CudaMemoryProfiler, flatten_2d_lists,
+from vllm.utils import (CudaMemoryProfiler, async_tensor_h2d, flatten_2d_lists,
                         get_kv_cache_torch_dtype, is_hip,
                         is_pin_memory_available)
 from vllm.worker.model_runner_base import (
